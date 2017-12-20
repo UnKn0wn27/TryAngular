f6cabd3116f74d5b8f6d0a9cd55fa976c17051f6
omuntean <omuntean.ebs@gmail.com>
2017-12-20 11:27:50 +0200

    Try Angular

---
+++ src/js/app/app.module.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('try', [
+  // external
+  'angularUtils.directives.dirPagination',
+  'ngResource',
+  'ngRoute',
+  'ui.bootstrap',
+
+  // internal
+  'blogDetail',
+  'blogList',
+  'confirmClick',
+  'tryNav'
+]);
